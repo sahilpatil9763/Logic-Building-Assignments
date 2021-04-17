37f04b6f14f888e2280d132c14d6f97f45bfb88f
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2021-04-17 15:37:43 +0530

    Assignment 3

---
+++ Assignment 3/03) Accept number form user and print even factors of that number.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+
+void DisplayFactor(int iNo)
+{
+    int i = 0;
+
+    if(iNo <= 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(i=1; i<=iNo/2; i++)
+    {
+        if(iNo%i == 0 && i%2 == 0)
+        {
+            printf("%d",i);
+        }
+    }
+
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("\n Enter number : ");
+    scanf("%d",&iValue);
+
+    DisplayFactor(iValue);
+
+    return 0;
+}
---
+++ Assignment 3/04) Accept one character from user and convert case of that character.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+int DisplayConvert(char cValue)
+{
+    if(cValue >= 97 && cValue <= 122)
+    {
+        cValue = cValue + 32;
+        printf("%c\n",cValue);
+    }
+
+    else if(cValue >= 'A' && cValue <= 'Z')
+    {
+        cValue = cValue + 32;
+        printf("%c\n",cValue);
+    }
+}
+
+int main()
+{
+    char cValue = '\0';
+    printf("Enter character\n");
+    scanf("%c",&cValue);
+
+    DisplayConvert(cValue);
+
+    return 0;
+}
