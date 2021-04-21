8bf6986a79b0d71ad43dec9bd26c0781c6114804
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2021-04-21 21:39:32 +0530

    Add files via upload

---
+++ Assignment 11/01) Accept range and display all numbers in between that range.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+void RangeDisplay(int iStart,int iEnd)
+{
+    if(iStart >= iEnd)
+    {
+        printf("\n Invalid range");
+    }
+    for(int i = iStart ; i <= iEnd ; i++)
+        {
+            printf(" %d ",i);
+        }
+}
+int main()
+{
+    int iValue1 = 0,iValue2 = 0;
+
+    printf("\nEnter Starting Point : ");
+    scanf("%d",&iValue1);
+
+    printf("\nEnter Ending Point : ");
+    scanf("%d",&iValue2);
+
+    RangeDisplay(iValue1,iValue2);
+
+    getch();
+    return 0;
+}
