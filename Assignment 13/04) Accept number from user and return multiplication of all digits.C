d29ecf8bbd9582706e32de903ab62575fc4629db
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2021-04-23 21:43:30 +0530

    Add files via upload

---
+++ Assignment 13/04) Accept number from user and return multiplication of all digits.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+
+int MultDigits(int iNo)
+{
+  int Temp = 0 , Dig = 0, mult = 1;
+
+  for(Temp = iNo ; Temp != 0 ; Temp = Temp / 10)
+  {
+      Dig = Temp % 10;
+      {
+        mult = mult * Dig;
+      }
+  }
+  return mult;
+}
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("\nEnter a number : ");
+    scanf("%d",&iValue);
+
+    iRet = MultDigits(iValue);
+
+    printf("multiplication of digits of %d is %d",iValue,iRet);
+
+    getch();
+    return 0;
+}
