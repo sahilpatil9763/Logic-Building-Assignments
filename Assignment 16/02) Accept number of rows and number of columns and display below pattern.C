712ea7dd24ea0436ad3e174c5c79ef5b11f98e8a
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2021-04-27 14:40:15 +0530

    Add files via upload

---
+++ Assignment 16/02) Accept number of rows and number of columns and display below pattern.C
@@ -0,0 +1,47 @@
+/*
+   Input  : iRow = 4     iCol = 4
+   Output : A A A A
+            B B B B
+            C C C C
+            D D D D
+*/
+
+
+
+#include<stdio.h>
+#include<conio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    int ch = 0;
+    for(int i = 1 ; i <= iRow ; i++)
+    {
+        for(int j = 1 ; j <= iCol ; j++)
+        {
+            if(i % 2 != 0)
+            {
+                ch = 64;
+                printf(" %c ",ch+j);
+            }
+            else
+            {
+                ch = 96;
+                printf(" %c ",ch+j);
+            }
+        }
+
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1 = 0 , iValue2 = 0;
+
+    printf("\n Enter number of rows and columns elements : ");
+    scanf("%d%d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    getch();
+    return 0;
+}
