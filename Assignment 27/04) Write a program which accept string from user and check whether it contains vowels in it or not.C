145f2d3d9b3a1d4ea59e93b41aed1578906a79ab
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2021-05-14 11:24:43 +0530

    Assignment 27

---
+++ Assignment 27/04) Write a program which accept string from user and check whether it contains vowels in it or not.C
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+int ChkVowel(char *str)
+{
+    int iCnt = 0;
+
+    while(*str != '\0')
+    {
+        if(*str == 'a' || *str == 'A' || *str == 'e' || *str == 'E' || *str == 'i' || *str == 'I' || *str == 'o' || *str == 'O' || *str == 'u' || *str == 'U')
+        {
+            return TRUE;
+        }
+      str++;
+    }
+
+    return iCnt;
+}
+
+int main()
+{
+    char arr[20]=" ";
+    BOOL bRet = FALSE;
+
+    printf("\n Enter String : ");
+    scanf("%[^'\n']s",arr);
+
+    bRet = ChkVowel(arr);
+
+    if(bRet == TRUE)
+    {
+        printf("\n Contains Vowel.");
+    }
+    else
+    {
+        printf("\n There is no Vowel");
+    }
+
+    getch();
+    return 0;
+}
