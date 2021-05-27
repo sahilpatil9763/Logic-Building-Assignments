c5b080d0ded0eb6b2cf24501654812f9796ee576
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2021-05-27 13:20:33 +0530

    Assignment 37

---
+++ Assignment 37/05) Replace each of first character of each word into capital case.C
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<conio.h>
+
+void StrCap(char *cstr)
+{
+    if(cstr == 0)
+    {
+        printf("\n Empty String......");
+        return ;
+    }
+
+    while(*cstr != '\0')
+    {
+        if(*cstr == ' ')
+        {
+            cstr++;
+            continue;
+        }
+
+        if(*cstr >= 'a' && *cstr <= 'z')
+        {
+            *cstr = *cstr - 32;
+            *cstr++;
+        }
+
+        while(*cstr != ' ' && *cstr != '\0')
+        {
+            cstr++;
+        }
+    }
+    return;
+
+}
+int main()
+{
+    char str[30]="\0" , dest[30] = "\0";
+
+    printf("\n Enter The String : ");
+    scanf(" %[^\n]s",str);
+
+    StrCap(str);
+
+    printf("%s",str);
+
+    getch();
+    return 0;
+}
